f694614a6c7c9bae49095f59d0a81f57312fcafd
Joseph Semlak <semlak@gmail.com>
2018-01-08 20:40:56 -0600

    remove colored tiles after image load, tried to clean up all sorts of code, improved messages in tab pane while loading, ...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -31,13 +31,24 @@ function shuffleArray(arr) {
 	return outputArr;
 }
 
+// function for getting somewhat random color. Used to make image tiles look pretty while loading
+// Function will not same color twice i n a row
+var previousColor = null;
+let colors = ["#77B300", "#9933CC" , "#CC0000", "#3366cc", "#F5E625", "#FF8C00", "#00FFFF", "#FF1493"];
+
+var randomColor = function() {
+	let filteredColors = colors.filter(color => color !== previousColor);
+	previousColor = filteredColors[Math.floor(Math.random()*filteredColors.length)]
+	return previousColor;
+};
+
+
 var app = null;
 
 
 
 let GiphyApp = class {
 	constructor() {
-		// let topics = [];
 		let me = this;
 		$(document).on("click", ".btn-load-images", me.loadTopicImages);
 		$(document).on("click", ".topic-search-button", me.addTopic);
@@ -46,31 +57,21 @@ let GiphyApp = class {
 		$(document).on("click", "#search-box-x", me.clearTopicSearch);
 		$(document).on("keydown", "#new-topic", me.enableClearSearchButton);
 		$(document).on("click", "#images-tab-bar .close", me.closeTab);
-		// $(document).on('popover', '[data-toggle="popover"]',{trigger: 'hover'})
-			// $('[data-toggle="popover"]').popover({trigger: 'hover'});
-		// $(document).on('popover', '[data-toggle="popover"]',{trigger: 'hover'})
-
-			// $('[data-toggle="popover"]').popover({trigger: 'hover'});
+		$(document).popover({selector: '[rel=popover]'});
 
 		me.initialTopics = ["Kitten", "Puppy", "Star Trek", "Xena", "JavaScript"];
 		me.topics = me.initialTopics.slice(0);
-		// this.theme = theme;
 		me.setupAppDivs();
 		me.gifLimit = initilGifLimit;
 		me.addSearchPanel();
 		me.renderTopicButtons();
 		me.paneIDs = [];
 
-		// }, 0);
 	}
 
 	reset() {
 		app.topics = [];
-		// app.topics = app.initialTopics.slice(0);
 		app.renderTopicButtons();
-		$(".images-container")
-			.empty()
-			.append($("<div>").addClass("col").text("Click one of the topics to load gifs!"));
 	}
 
 	closeTab() {
@@ -89,7 +90,7 @@ let GiphyApp = class {
 			paneID = paneID.replace(/#/, "");
 			app.paneIDs = app.paneIDs.filter(id => id !== paneID);
 
-		}, 0);
+		}, 10);
 
 	}
 
@@ -101,6 +102,10 @@ let GiphyApp = class {
 
 	}
 
+	gifLoaded() {
+		$(this).removeClass("image-loading").addClass("image-loaded");
+
+	}
 	enableClearSearchButton() {
 		$("#search-box-x").removeClass("disabled")
 	}
@@ -121,30 +126,22 @@ let GiphyApp = class {
 	}
 
 	loadTopicImages(event) {
-		// console.log(event);
-		// console.log($(this).text())
 		let topic = $(this).text();
 		console.log(topic);
 		app.loadGifs(topic);
 		setTimeout(function() {
-			// $('.gif').popover({trigger: 'hover'}, 100);
-			  // $('[data-toggle="popover"]').popover()
-			// $(".gif").popover('enable');
-			// $(".gif").popover({trigger: 'hover'});
 			$('[data-toggle="popover"]').popover({trigger: 'hover'});
 
 		}, 1000);
 	}
 
 	addTopic(event) {
-		// console.log($(this));
 		event.preventDefault();
 		let newTopic = $("#new-topic").val().trim();
 		console.log(newTopic);
 		if (newTopic.trim().length > 0) {
 			if (app.topics.filter(topic => topic.toLowerCase().replace(/ /, "") === newTopic.toLowerCase().replace(/ /, "")).length < 1) {
 				app.topics.push(newTopic);
-				// $("#new-topic").attr("value", "");
 				$("#new-topic").val("");
 				$("#search-box-x").addClass("disabled")
 				$("#new-topic").removeClass("is-invalid");
@@ -153,7 +150,6 @@ let GiphyApp = class {
 			else {
 				$("#new-topic").addClass("is-invalid")
 				$("#topic-search-feedback-message").text("That topic is already listed!")
-
 			}
 		}
 		else {
@@ -168,6 +164,11 @@ let GiphyApp = class {
 		let currentTabBarLength = $("#images-tab-bar").children().length;
 		if (currentTabBarLength > 0) {
 			$("#images-tab-bar").removeClass("hidden");
+		}
+
+		// remove any header still left in tabbar
+		if ($(".tab-content").html() === "<h3>Click one of the topics to load gifs!</h3>") {
+			$(".tab-content").empty();
 		}
 
 		let key = tabTitle.toLowerCase().replace(/ /g, "");
@@ -197,11 +198,10 @@ let GiphyApp = class {
 		$(".tab-content")
 				.append($("<div>").addClass("tab-pane active")
 					.attr("id", newPaneID)
-					// .attr("href", "#")
 					.attr("role", "tabpanel")
 					.attr("aria-labeledby", "images-tab-" + tabTitle.toLowerCase())
 					.append($("<div>").addClass("images-container")
-						.text((initialText || "Loading..."))
+						.html("<h3>" + (initialText || "Loading...") + "</h3>")
 					)
 				)
 		return newPaneID;
@@ -210,10 +210,11 @@ let GiphyApp = class {
 	createInitialImageTabBarAndPane() {
 		let tabDivs = $("<div>")
 		.append($("<div>").addClass("row")
-			.append($("<ul>").addClass("nav nav-tabs hidden").attr("id", "images-tab-bar").attr("role", "tablist"))
+			.append($("<ul>").addClass("nav nav-tabs hidden")
+				.attr("id", "images-tab-bar").attr("role", "tablist"))
 		)
 		.append($("<div>").addClass("row")
-			.append($("<div>").addClass("tab-content"))
+			.append($("<div>").addClass("tab-content").html("<h3>Click one of the topics to load gifs!</h3>"))
 		)
 
 		return tabDivs;
@@ -221,14 +222,14 @@ let GiphyApp = class {
 
 	setupAppDivs() {
 		let tabbedDivs = this.createInitialImageTabBarAndPane();
-		$("#app-container").
-			append($("<div>").addClass("row flex-column-reverse flex-md-row").
-				append($("<div>").addClass("col-xl-8 col-lg-8 col-md-7 col-sm-push-5" ).
-					append($("<div>").addClass("topic-buttons-container")
+		$("#app-container")
+			.append($("<div>").addClass("row flex-column-reverse flex-md-row")
+				.append($("<div>").addClass("col-xl-8 col-lg-8 col-md-7 col-sm-push-5" )
+					.append($("<div>").addClass("topic-buttons-container")
 				)
-			).
-			append($("<div>").addClass("col-xl-4 col-lg-4 col-md-5 col-sm-pull-7").
-				append($("<div>").addClass("search-container"))
+			)
+			.append($("<div>").addClass("col-xl-4 col-lg-4 col-md-5 col-sm-pull-7")
+				.append($("<div>").addClass("search-container"))
 			)
 		).append(tabbedDivs)
 
@@ -238,9 +239,7 @@ let GiphyApp = class {
 	addSearchPanel() {
 		let searchPanel = ($("<div>").addClass("card border-info"))
 		searchPanel.append($("<div>").addClass("card-header").text("Add topic"));
-		// $("#searchPanel"));
 		let cardBody = $("<div>").addClass("card-body");
-		// cardBody.append($("<i>").addClass("fa fa-close").attr( "aria-hidden", true).attr("id", "search-box-x"));
 
 		cardBody.append($("<form>").addClass("form-group")
 			.prop("novalidate", true)
@@ -248,11 +247,8 @@ let GiphyApp = class {
 			.append($("<div>").addClass("form-group")
 				.append($("<input>").addClass("form-control form-control-lg ")
 					.attr("type", "text").attr("id", "new-topic").attr("placeholder", "Topic to add")
-					// .css({"position": "relative"})
-					// .prop("required", true)
 				)
 				.append($("<span>").addClass("clear-search fa fa-close disabled").attr("id", "search-box-x")
-					// .html("<i class=\"fa fa-close\" aria-hidden=\"true\"></i>")
 				)
 				.append($("<div>").addClass("invalid-feedback")
 					.text("Please enter a valid topic.")
@@ -261,17 +257,12 @@ let GiphyApp = class {
 			)
 			.append($("<button>").addClass("btn btn-info square topic-search-button")
 				.html("<i class=\"fa fa-search\" aria-hidden=\"true\"></i>&nbsp;&nbsp; Add topic")
-				// text("Add topic").
 				.attr("autofocus", true).attr("type", "submit")));
 
 		searchPanel.append(cardBody);
 
-		// searchPanel
-		// .append($("<span>").addClass("clear-search").attr("id", "search-box-x")
-		// .html("<i class=\"fa fa-close\" aria-hidden=\"true\"></i>"))
 
 		$(".search-container").append(searchPanel);
-		// <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
 	}
 
 
@@ -285,10 +276,11 @@ let GiphyApp = class {
 		me.topics.forEach(function(topic, i) {
 			console.log(topic);
 			let topicButton = $("<div>").addClass("btn square btn-primary btn-load-images").text(topic).attr("value", topic);
-			topicButton.attr("data-toggle", "popover").attr("data-content", "Load images relating to " + topic).attr("data-placement", "bottom").
-			attr("delay", 500)
-// <button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
-
+			topicButton
+				.attr("data-toggle", "popover")
+				.attr("data-content", "Load images relating to " + topic)
+				.attr("data-placement", "bottom")
+				.attr("delay", 500);
 			$(".topic-buttons-container").append(topicButton);
 		});
 		setTimeout(function() {
@@ -299,17 +291,6 @@ let GiphyApp = class {
 	}
 
 	appendGifs(data, newPaneID) {
-
-		var previousColor = null;
-		// let colors = ["rgb(0, 255, 153)", "rgb(153, 51, 255)", "rgb(255,102,102)", "rgb(0, 204, 255)", "rgb(255, 243, 92)"];
-		let colors = ["#77B300", "#9933CC" , "#CC0000", "#3366cc", "#F5E625", "#FF8C00", "#00FFFF", "#FF1493"];
-
-		var randomColor = function() {
-			let filteredColors = colors.filter(color => color !== previousColor);
-			previousColor = filteredColors[Math.floor(Math.random()*filteredColors.length)]
-			return previousColor;
-		};
-
 		$("#" + newPaneID + " .images-container").empty();
 		// shuffle order of images to make it seem like images being loaded are not the same every time
 		let shuffledItems = shuffleArray(data);
@@ -319,7 +300,7 @@ let GiphyApp = class {
 			let cardID = "gif-card-" + item.id;
 			let newCard = $("<div>").addClass('card gif-card')
 						.attr("id", cardID);
-			let image = $("<img>").addClass("card-image-bottom  gif img-fluid")
+			let image = $("<img>").addClass("card-image-bottom  gif img-fluid image-loading")
 					.attr("data-still", data["data-still"])
 					.attr("src", item.images.fixed_width_still.url)
 					.attr("data-still", item.images.fixed_width_still.url)
@@ -330,34 +311,22 @@ let GiphyApp = class {
 
 			let imageWrapper = $("<div>").addClass("image-wrapper");
 			imageWrapper.append(image);
-			let imageCardBody = ($("<div>").addClass("card-body").
-				css("background-color", randomColor()).
-				append($("<div>").addClass("gif-rating").text("Rating: " + item.rating.toUpperCase() ))
-				// .append(image));
+			let imageCardBody = ($("<div>").addClass("card-body")
+				.css("background-color", randomColor())
+				.append($("<div>").addClass("gif-rating").text("Rating: " + item.rating.toUpperCase() ))
 				.append(imageWrapper));
-			// console.log("using ratio" , heightToWidthRatio + "%");
 			// newCard
 
 			newCard.append(imageCardBody);
 			$("#" + newPaneID + " .images-container").append(newCard);
-			// let cardWidth = newCard.outerWidth();
-			// let imageHeightToSet = cardWidth * item.images.fixed_width_still.height / item.images.fixed_width_still.width;
-			// newCard.css({"padding-bottom": heightToWidthRatio + "%"});
-			// image.height(imageHeightToSet);
-
-
 			newCard.append('<style>#' + cardID + ' .image-wrapper:before{content: ""; display: block; width: 100%; padding-top: ' + heightToWidthRatio + '%;}</style>');
-
 		})
+
+		$("#" + newPaneID + " .images-container .gif").on("load", app.gifLoaded)
 
 	}
 
 	loadGifs(topic) {
-		// let theme = "animals"
-		// let url = "https://media1.giphy.com/";
-		// let url = "https://api.giphy.com/v1/gifs/search?api_key=c5CF9X0IcrkjEmQ6MkiQE6V7D7oo2QAN&q=" + theme + "+" + topic.replace(" ", "+") + "&limit="+ app.gifLimit + "&offset=0&lang=en"
-		let url = "https://api.giphy.com/v1/gifs/search?api_key=c5CF9X0IcrkjEmQ6MkiQE6V7D7oo2QAN&q=" + topic.replace(" ", "+") + "&limit="+ app.gifLimit + "&offset=0&lang=en"
-		// $(".images-container").empty().text("Loading images...");
 		let key = topic.toLowerCase().replace(/ /g, "");
 		let paneID = "tab-pane-" + key;
 		let tabID = "images-tab-" + key;
@@ -371,28 +340,23 @@ let GiphyApp = class {
 			return;
 		}
 
-
-
-
-		let newPaneID = this.appendNewTabToImageTabBarAndPane(topic, "Loading images...")
-		console.log("newPaneID: ", newPaneID);
+		let newPaneID = this.appendNewTabToImageTabBarAndPane(topic, "Retrieving image data...")
+		let url = "https://api.giphy.com/v1/gifs/search?api_key=c5CF9X0IcrkjEmQ6MkiQE6V7D7oo2QAN&q=" +
+			topic.replace(" ", "+") + "&limit="+ app.gifLimit + "&offset=0&lang=en"
 
 		$.ajax({
 			url: url,
 			method: "GET"
 		}).done(function(response) {
 			let data = response.data;
-			// $(".images-container").empty();
 			if (data.length>0) {
 				app.appendGifs(data, newPaneID);
 			}
 			else {
 				// no images were received with response
-				$("#" + newPaneID + " .images-container").empty().text("No images found related to \"" + topic + "\"");
+				$("#" + newPaneID + " .images-container").empty().html("<h3>No images found related to \"" + topic + "\".</h3>");
 			}
-
 		})
-
 	}
 }
 
@@ -405,28 +369,17 @@ $("#options-submit").on("click", function(e) {
 
 // for slider, modified from https://stackoverflow.com/questions/24463678/where-can-i-find-bootstrap-styles-for-html5-range-inputs
 $("#rangeinput").on("change", function(e) {
-	// console.log($(this).val());
 	$(this).val($(this).val());
-// onchange="rangevalue.value=value"
 
 })
 
 $("#rangeinput").on("mousemove", function(e) {
-	// if ()
 	$("#rangevalue").text($(this).val())
 })
 
 $(document).ready(function() {
 	app = new GiphyApp();
-	// $('[data-toggle="popover"]').popover({trigger: 'hover'});
-	// $(".gif").popover({trigger: 'focus'});
-	$(document).popover({
-    	selector: '[rel=popover]'
-	});
-	// $(document).popover(function(e) {
-	// 	console.log("Hey found a popover");
-	// })
-// add new popover
+	// $(document).popover({selector: '[rel=popover]'});
 
 
 })
